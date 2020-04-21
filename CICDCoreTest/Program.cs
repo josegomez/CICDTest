b0c5224f205a32d8fff9c79e4ccf339d56d9f0ac
Jose C Gomez <jose@josecgomez.com>
2020-04-21 17:58:08 -0400

    Add project files.

---
+++ CICDCoreTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CICDCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
